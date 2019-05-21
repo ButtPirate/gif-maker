4923a0994da6f769d830d0c669d2f68fe5477258
Butt Pirate <buttpirate@tutanota.com>
2019-05-21 21:19:06 +0300

    Refactoring

--- src/main/java/ru/bpirate/ResourceService.java
+++ src/main/java/ru/bpirate/ResourceService.java
@@ -9,9 +9,8 @@ import java.io.OutputStream;
 public class ResourceService {
     public static void exportResource(String resourceName, String folderPath) throws BackendException {
         System.out.println("Unpacking resource <" + resourceName + ">...");
-        InputStream stream = ResourceService.class.getResourceAsStream(resourceName);//note that each / is a directory down in the "jar tree" been the jar the root of the tree
+        InputStream stream = ResourceService.class.getResourceAsStream(resourceName);//note that each / is a directory down in the "jar tree" with jar being the root of the tree
         OutputStream resStreamOut = null;
-        String jarFolder;
         int readBytes;
         byte[] buffer = new byte[4096];
         try {
